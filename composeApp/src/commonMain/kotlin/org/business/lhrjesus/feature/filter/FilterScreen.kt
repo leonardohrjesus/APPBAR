4d2f01fdc0202306058bae1d7d3ae6e9e7254aa9
Leonardo Jesus <leonardo.hrjesus>
2025-05-13 22:44:11 -0300

    tela de detalhes

--- composeApp/src/commonMain/kotlin/org/business/lhrjesus/feature/filter/FilterScreen.kt
+++ composeApp/src/commonMain/kotlin/org/business/lhrjesus/feature/filter/FilterScreen.kt
@@ -1,15 +1,22 @@
 package org.business.lhrjesus.feature.filter
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.LocationOn
 import androidx.compose.material.icons.filled.Place
 import androidx.compose.material.icons.filled.ShoppingCart
 import androidx.compose.material3.BottomAppBar
@@ -30,8 +37,15 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import appbar.composeapp.generated.resources.Res
+import appbar.composeapp.generated.resources.icon_instagram
+import coil3.compose.AsyncImage
+import org.jetbrains.compose.resources.painterResource
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -88,7 +102,7 @@ fun FilterScreen(
                                         Icon(
                                             Icons.Filled.Place,
                                             contentDescription = "Mapa",
-                                           )
+                                        )
                                     }
                                     Text(text = "Mapa")
                                 }
@@ -109,11 +123,12 @@ fun FilterScreen(
                 )
 
     ) { innerPadding ->
-      Box(contentAlignment = Alignment.Center,
-          modifier = Modifier.padding(50.dp)
-            ){
-          AboutComponent("Leonardo", "MONSTRO",onBackPressed)
-      }
+        Box(contentAlignment = Alignment.Center,
+            modifier = Modifier.padding(50.dp)
+        ){
+            EventDetailScreen(navigateToMapsScreen)
+            //AboutComponent("Leonardo", "MONSTRO",onBackPressed)
+        }
     }
 
 }
@@ -156,9 +171,134 @@ private fun AboutComponent(
 
 }
 
-private fun makeItems():List<Pair<String,String>>{
-    return listOf(
-        Pair("Sistema operacional","TESTE"),
-        Pair("Dispositivo", "TESTE DISPOSITIVO")
-    )
+
+@Composable
+fun EventDetailScreen(navigateToMapsScreen: () -> Unit) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        // Banner Image
+
+        AsyncImage(
+            model = "https://lh3.googleusercontent.com/p/AF1QipOdntTx9QXDxQtZ5tXJVOC4lCEPT5LSsV24aaE2=s1360-w1360-h1020",
+            contentDescription = "imagem",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            contentScale = ContentScale.FillBounds // ou ContentScale.FillWidth se quiser mostrar tudo
+        )
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Event Information
+        Text(
+            text = "Pagode do Japa",
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold,
+            modifier = Modifier.padding(horizontal = 16.dp)
+        )
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(4.dp)
+        ) {
+
+            Row(
+                modifier = Modifier.padding(start = 10.dp)
+            ) {
+                Text(
+                    text = "Bar 567 ",
+                    fontSize = 14.sp
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .clickable {
+                        navigateToMapsScreen.invoke()
+                    }
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.LocationOn,
+                    contentDescription = "Ícone de localização",
+                    tint = Color.Black,
+                )
+                Text(
+                    text = "R. Aspicuelta, 567 - Vila Madalena",
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 12.sp
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.DateRange,
+                    contentDescription = "Ícone de Data",
+                    tint = Color.Black,
+
+                    )
+                Text(
+                    text = "Sab, 16  de Abril - 18:30",
+                    modifier = Modifier.padding(start = 8.dp),
+                    fontSize = 12.sp
+                )
+            }
+
+            Row(
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                InstagramIcon()
+            }
+
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Ticket Purchase Button
+        Button(
+            onClick = { /* Lógica de compra */ },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp)
+        ) {
+            Text(text = "Comprar Ingresso")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+    }
 }
+
+@Composable
+fun InstagramIcon() {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .clickable {
+                // Ação ao clicar no ícone do Instagram
+            }
+    ) {
+        Image(
+            painter = painterResource(Res.drawable.icon_instagram),
+            contentDescription = "Ícone do Instagram",
+            modifier = Modifier.size(24.dp)
+        )
+        Text(
+            text = "Siga-nos no Instagram",
+            modifier = Modifier.padding(start = 8.dp),
+            fontSize = 12.sp,
+            color = Color.Black
+        )
+    }
+
+}
+
+
+
+
